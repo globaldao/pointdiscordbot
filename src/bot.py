3a7e681455942a3d0b69ff1c2e3214c71ea3ec60
Sebastián Téllez <juantellez@gootechnology.com>
2019-07-02 15:42:56 -0500

    cambios

--- src/bot.py
+++ src/bot.py
@@ -364,9 +364,8 @@ async def on_message(message):
     if AT_BOT in message.content or (message.author.id != client.user.id):
         try:
             #if not message.channel.is_private:
-            print(message)
-            message.content = message.content.replace(AT_BOT, '', 1)
-            await client.send_typing(message.channel)
+                message.content = message.content.replace(AT_BOT, '', 1)
+            #await client.send_typing(message.channel)
             await handle_message(message)
         except socket_error as serr:
             if serr.errno != errno.ECONNREFUSED:
